c04076e56602679acd7bf280c71e16f3eecf3f82
TopL0ader <53130480+TopL0ader@users.noreply.github.com>
2020-05-18 01:15:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -140,7 +140,7 @@ app.post('/:username/:platform/:leagueId/freeagents/roster', (req, res) => {
     req.on('end', () => {
         const { rosterInfoList } = JSON.parse(body);
         const dataRef = ref.child(
-            `data/${username}/${leagueId}/players/${rosterId}`
+            `data/${username}/${leagueId}/players/${roster.rosterId}`
         );
         const players = {};
         rosterInfoList.forEach(player => {
@@ -170,7 +170,7 @@ app.post('/:username/:platform/:leagueId/team/:teamId/roster', (req, res) => {
     req.on('end', () => {
         const { rosterInfoList } = JSON.parse(body);
         const dataRef = ref.child(
-            `data/${username}/${leagueId}/players/${rosterId}`
+            `data/${username}/${leagueId}/players/${roster.rosterId}`
         );
         const players = {};
         rosterInfoList.forEach(player => {
